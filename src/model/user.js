f7f699ebe2022327ce4cfe3dc0396e81c1d517b8
vuduchiieu <vuduchiieu@gmail.com>
2024-05-02 23:10:22 +0700

    fix user

--- src/model/user.js
+++ src/model/user.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  _id: String,
-  username: String,
-  email: String,
-  password: String,
-  fullname: String,
-  phone: Number,
-  detailedAddress: String,
-  town: String,
-  city: String,
-  postcode: Number,
-});
+const userSchema = new mongoose.Schema(
+  {
+    username: String,
+    email: String,
+    password: String,
+    fullname: String,
+    phone: Number,
+    detailedAddress: String,
+    town: String,
+    city: String,
+    postcode: Number,
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("users", userSchema);
 
