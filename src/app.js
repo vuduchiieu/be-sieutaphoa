292b701d10542744cdf375f8d2b719c0566e35cc
vuduchiieu <vuduchiieu@gmail.com>
2024-05-02 18:40:35 +0700

    fix cors

--- src/app.js
+++ src/app.js
@@ -11,12 +11,21 @@ dotenv.config();
 const app = express();
 app.use(morgan("combined"));
 
-app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(
   cors({
     origin: "*",
   })
 );
+
+app.use(express.json());
 
 app.use("/v1/auth", authRouter);
 
