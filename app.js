326939ca48e79d16e5bf1375d74869b20ea0ecb5
vuduchiieu <vuduchiieu@gmail.com>
2024-05-02 19:26:14 +0700

    fix 2

--- app.js
+++ app.js
@@ -2,30 +2,17 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
-import authRouter from "./routes/authRoute.js";
 import morgan from "morgan";
 
+import authRouter from "./src/routes/authRoute.js";
+
 dotenv.config();
-
 const app = express();
-app.use(morgan("combined"));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 
 app.use(express.json());
+app.use(morgan("combined"));
 
 app.use("/v1/auth", authRouter);
 
